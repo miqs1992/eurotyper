b7b3a26c4431747691fd86cc52566c2f3ad3ca58
Wojciech Mikusek <w.mikusek@exlabs.pl>
2022-11-11 09:50:46 +0100

    Add reseting password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  #
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['SMPT_USER'],
+    password: ENV['SMPT_PASSWORD']
+  }
+
+  config.action_mailer.default_url_options = { host: ENV['RENDER_EXTERNAL_URL'] }
 end
